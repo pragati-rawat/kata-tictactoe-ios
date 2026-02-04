efb9434876c5f284225a62e50ee18439902ab951
Pragati Rawat <pragatidubey2011@gmail.com>
2026-02-04 17:29:02 +0530

    feet: Added GameRulesEngine Service class for business logic of games rules

---
+++ TicTacToe/TicTacToe/Domain/Domain-Service/GameRulesEngine.swift
@@ -0,0 +1,13 @@
+//
+//  GameRulesEngine.swift
+//  TicTacToe
+//
+//  Created by Pragati RAWAT on 04/02/2026.
+//
+import Foundation
+
+final class GameRulesEngine: GameRulesEngineProtocol {
+    func evaluate(board: Board) -> GameResult {
+        .ongoing
+    }
+}
